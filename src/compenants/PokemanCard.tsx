8d5c2ab013c77f782678d62ded1efd98519144bc
JULIAN HAZARD <hazardjulian74@gmail.com>
2024-12-11 12:06:57 +0100

    NODE02

---
+++ src/compenants/PokemanCard.tsx
@@ -0,0 +1,15 @@
+function PokemanCard() {
+	return (
+		<figure>
+			<img
+				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+				alt="Bulbasaur"
+			/>
+			<figcaption>
+				<p>Bulbasaur</p>
+			</figcaption>
+		</figure>
+	);
+}
+
+export default PokemanCard;
